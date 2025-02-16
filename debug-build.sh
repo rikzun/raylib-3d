330d46473c031ec2c2d03a752369f7f66fa1088a
Михаил Якименко <rik.zunqq@gmail.com>
2025-02-16 20:36:53 +0300

    tasks fix

--- debug-build.sh
+++ debug-build.sh
@@ -2,10 +2,10 @@
 
 WORKSPACE=$(dirname "$(realpath $0)");
 OUTPUT_FOLDER=$WORKSPACE/output
-OUTPUT_FILE=$OUTPUT_FOLDER/debug-application.exe
+OUTPUT_FILE=$OUTPUT_FOLDER/debug.exe
 SOURCE_FOLDER=$WORKSPACE/src
 INPUT_FILE=$SOURCE_FOLDER/main.cpp
 
 cd C:/w64devkit/bin/;
 mkdir -p $OUTPUT_FOLDER;
-gcc -o $OUTPUT_FILE $INPUT_FILE -lraylib -lgdi32 -lwinmm
+./gcc -o $OUTPUT_FILE $INPUT_FILE -lraylib -lgdi32 -lwinmm
